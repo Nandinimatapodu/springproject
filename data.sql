40acfd0ad9caf67cc0b65b12b41398f2bf75acce
Nandinimatapodu <100038343+Nandinimatapodu@users.noreply.github.com>
2022-03-04 20:11:51 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,16 +2,17 @@ create table employee
 (
     id integer ,
     name varchar(255) not null,
+    age integer,
     salary integer,
     designation varchar(255),
     primary key(id)
 );
-insert into employee(id,name,salary,designation)
-values( 100,'Nandini ' ,40000,'programmer');
-insert into employee(id,name,salary,designation)
-values( 101,'lakshman ' ,50000,'Manager');
-insert into employee(id,name,salary,designation)
-values( 102,'apoorv ' ,60000,'seniormanager');
-insert into employee(id,name,salary,designation)
-values( 103,'Rajitha ' ,70000,'Tester');
+insert into employee(id,name,age,salary,designation)
+values( 100,'Nandini ',22 ,40000,'programmer');
+insert into employee(id,name,age,salary,designation)
+values( 101,'lakshman ',27 ,50000,'Manager');
+insert into employee(id,name,age,salary,designation)
+values( 102,'apoorv ' ,30,60000,'seniormanager');
+insert into employee(id,name,age,salary,designation)
+values( 103,'Rajitha ',28 ,70000,'Tester');
 select * from employee;
